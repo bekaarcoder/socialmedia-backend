379b82ea73594f88be4b4646216142d9187b67d8
Shashank <s_shashank@outlook.com>
2023-01-14 15:09:12 +0530

    added cloudinary function

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import cloudinary from "cloudinary";
 import { createLogger, format, transports, Logger } from "winston";
 
 const { combine, timestamp, label, printf } = format;
@@ -16,6 +17,9 @@ class Config {
     public SECRET_KEY_TWO: string | undefined;
     public CLIENT_URL: string | undefined;
     public REDIS_HOST: string | undefined;
+    public CLOUD_NAME: string | undefined;
+    public CLOUD_API_KEY: string | undefined;
+    public CLOUD_API_SECRET: string | undefined;
 
     private readonly DEFAULT_DATABASE_URL =
         "mongodb://0.0.0.0:27017/fbclone-backend";
@@ -29,11 +33,10 @@ class Config {
         this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || "";
         this.CLIENT_URL = process.env.CLIENT_URL || "";
         this.REDIS_HOST = process.env.REDIS_HOST || "";
+        this.CLOUD_NAME = process.env.CLOUD_NAME || "";
+        this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || "";
+        this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || "";
     }
-
-    // public createLogger(name: string): bunyan {
-    //     return bunyan.createLogger({ name, level: "debug" });
-    // }
 
     public createLogger(name: string): Logger {
         return createLogger({
@@ -54,6 +57,14 @@ class Config {
                 throw new Error(`Configuration ${key} is undefined`);
             }
         }
+    }
+
+    public cloudinaryConfig(): void {
+        cloudinary.v2.config({
+            cloud_name: this.CLOUD_NAME,
+            api_key: this.CLOUD_API_KEY,
+            api_secret: this.CLOUD_API_SECRET,
+        });
     }
 }
 
